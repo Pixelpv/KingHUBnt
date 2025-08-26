a814bc6c75a000f5fd31cf996245f8ced7cc2da6
Night <mateuzinho4866@gmail.com>
2025-08-26 20:02:07 -0300

    Create trazer.lua

---
+++ trazer.lua
@@ -0,0 +1,6 @@
+return function(tab)
+    tab:NewButton("Trazer Item", "Move o item selecionado para você", function()
+        print("Item trazido!")
+        -- Código de teleporte local
+    end)
+end
