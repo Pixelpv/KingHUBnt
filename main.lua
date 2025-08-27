e63f66d8c56c58ffcb8c5930fb9bffdb66a280fb
Night <mateuzinho4866@gmail.com>
2025-08-26 21:55:41 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,18 +1,15 @@
--- main.lua
 local function import(file)
     local url = "https://raw.githubusercontent.com/Pixelpv/KingHUBnt/main/" .. file
     return loadstring(game:HttpGet(url))()
 end
 
--- Garante que o CoreGui está pronto antes de criar UI
-task.spawn(function()
-    local player = game.Players.LocalPlayer
-    while not player or not player.PlayerGui do
-        task.wait(0.1)
-    end
-    -- Delay extra pra mobile
-    task.wait(1)
-    
-    local UI = import("ui.lua")
-    UI:Init()
-end)
+-- Espera o PlayerGui carregar
+local player = game.Players.LocalPlayer
+while not player or not player:FindFirstChild("PlayerGui") do
+    task.wait(0.1)
+end
+
+task.wait(1) -- delay extra pra mobile
+
+local UI = import("ui.lua")
+UI:Init(player.PlayerGui) -- passa PlayerGui para o UI
